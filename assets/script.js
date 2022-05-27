16df3b9c27558a3419a41ce20699ed5e8f5a4f9a
Athanasia <athanasiakotopoulos@gmail.com>
2022-05-27 18:51:40 -0400

    error handler

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,12 @@ var apiKey = "981e39b07d61fe2892bafaa16593aff8";
 var thisCity = "";
 var lastCity = "";
 
+
+//Error Handler
+
+var handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusTest);
+    }
+    return response;
+}
